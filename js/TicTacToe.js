5b04bf66d0e37109f22c14629200504dfcdfb2bc
TymekB <baran.tymoteusz@gmail.com>
2018-03-22 19:21:23 +0100

    added createBoard method

--- js/TicTacToe.js
+++ js/TicTacToe.js
@@ -1,13 +1,29 @@
 class TicTacToe {
     constructor(options = {}) {
-        this.board = options.board || 'body';
+
+        this.DOMElements = {
+            boardId: options.DOMElements.boardId,
+            fieldClass: options.DOMElements.fieldClass
+        };
+
         this.startingPlayer = options.startingPlayer || this.getRandomPlayer();
     }
+
 
     getRandomPlayer() {
         let players = ['X', 'O'];
         let randomVal = Math.floor(Math.random()*2);
 
         return players[randomVal];
+    }
+
+    createBoard() {
+        let htmlCode = '';
+
+        for(let i = 0; i < 9; i++) {
+            htmlCode += '<div class="'+this.DOMElements.fieldClass+'"></div>';
+        }
+
+        $("#"+this.DOMElements.boardId).html(htmlCode);
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-   let game = new TicTacToe();
+   let game = new TicTacToe({
+         DOMElements: {
+              boardId: "board",
+              fieldClass: "square"
+           }
+       });
 
    console.log(game);
+
+   game.createBoard();
 });
