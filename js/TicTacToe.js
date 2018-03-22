e87fe8447ce814d0d292bddca6bb0916f0b8174f
TymekB <baran.tymoteusz@gmail.com>
2018-03-22 22:49:42 +0100

    added checkIfPlayerWins method

--- js/TicTacToe.js
+++ js/TicTacToe.js
@@ -8,7 +8,8 @@ class TicTacToe {
 
         this.startingPlayer = options.startingPlayer || this.getRandomPlayer();
         this.currentPlayer = this.startingPlayer;
-        this.board = [];
+        this.board = ['n', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'];
+        this.winningPatterns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 4, 8, 2, 4, 6]
     }
 
     start() {
@@ -18,7 +19,7 @@ class TicTacToe {
 
     getRandomPlayer() {
         let players = ['X', 'O'];
-        let randomVal = Math.floor(Math.random()*2);
+        let randomVal = Math.floor(Math.random() * 2);
 
         return players[randomVal];
     }
@@ -26,38 +27,63 @@ class TicTacToe {
     createBoard() {
         let htmlCode = '';
 
-        for(let i = 0; i < 9; i++) {
-            let fieldId = "field"+i;
-            htmlCode += '<div class="'+this.DOMElements.fieldClass+'" id="'+fieldId+'"></div>';
+        for (let i = 0; i < 9; i++) {
+            let fieldId = "field" + i;
+            htmlCode += '<div class="' + this.DOMElements.fieldClass + '" id="' + fieldId + '"></div>';
         }
 
-        $("#"+this.DOMElements.boardId).html(htmlCode);
+
+        $("#" + this.DOMElements.boardId).html(htmlCode);
     }
 
     setPlayerOnBoard(fieldId) {
-        let field = $("#field"+fieldId);
+        let field = $("#field" + fieldId);
 
         field.html(this.currentPlayer);
         field.css('pointer-events', 'none');
 
-        if(this.currentPlayer === 'X') {
+        this.board[fieldId] = this.currentPlayer;
+        let winner = this.checkIfPlayerWins();
+
+        if (winner) {
+            alert(winner + " wins!");
+            $("." + this.DOMElements.fieldClass).css('pointer-events', 'none');
+            return;
+        }
+
+        if (this.currentPlayer === 'X') {
             this.currentPlayer = 'O';
         } else {
             this.currentPlayer = 'X';
         }
+    }
 
-        this.board[fieldId] = this.currentPlayer;
+    checkIfPlayerWins() {
+        let winner = false;
+
+        for (let i = 0; i < this.winningPatterns.length; i++) {
+            let field1 = this.board[this.winningPatterns[i]];
+            let field2 = this.board[this.winningPatterns[i + 1]];
+            let field3 = this.board[this.winningPatterns[i + 2]];
+
+            if (field1 === field2 && field2 === field3 && field1 !== 'n') {
+                winner = this.currentPlayer;
+            }
+        }
+
+        return winner;
     }
 
     addEventListeners() {
         let _this = this;
 
-        for(let i = 0; i < 9; i++) {
-            let fieldId = 'field'+i;
+        for (let i = 0; i < 9; i++) {
+            let fieldId = 'field' + i;
 
-            $("#"+fieldId).click(function(){
+            $("#" + fieldId).click(function () {
                 _this.setPlayerOnBoard(i);
             });
         }
+
     }
 }
