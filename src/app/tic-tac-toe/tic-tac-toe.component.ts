7360d7dcb688d0cdff745f6534ed9a85b1d0164e
TymekB <baran.tymoteusz@gmail.com>
2020-02-23 15:15:00 +0100

    added setting player on board

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -8,10 +8,22 @@ import {Component, OnInit} from '@angular/core';
 export class TicTacToeComponent implements OnInit {
 
     board = [];
+    player = 'X';
 
     constructor() {
-        for(let i = 0; i < 9; i++) {
-            this.board.push('n');
+        for (let i = 0; i < 9; i++) {
+            this.board.push(null);
+        }
+    }
+
+    setPlayerOnBoard(pos) {
+
+        this.board[pos] = this.player;
+
+        if (this.player === 'X') {
+            this.player = 'O';
+        } else {
+            this.player = 'X';
         }
     }
 
