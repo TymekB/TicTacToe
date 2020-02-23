48f0104b231b84a265d88495f91fd98cfd9325d9
TymekB <baran.tymoteusz@gmail.com>
2020-02-23 14:41:01 +0100

    added board

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-tic-tac-toe',
-  templateUrl: './tic-tac-toe.component.html',
-  styleUrls: ['./tic-tac-toe.component.scss']
+    selector: 'app-tic-tac-toe',
+    templateUrl: './tic-tac-toe.component.html',
+    styleUrls: ['./tic-tac-toe.component.scss']
 })
 export class TicTacToeComponent implements OnInit {
 
-  constructor() { }
+    board = [];
 
-  ngOnInit(): void {
-  }
+    constructor() {
+        for(let i = 0; i < 9; i++) {
+            this.board.push('n');
+        }
+    }
+
+    ngOnInit(): void {
+    }
 
 }
